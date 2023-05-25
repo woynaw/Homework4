39cb81f6924e7987a569b03a0e40431b669db0a1
Войналович Владислав <woynaw@hotmail.com>
2023-05-25 12:27:15 +0300

    add Zadacha25

--- Zadacha25/Program.cs
+++ Zadacha25/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool Check(int arg)
+{
+    if (arg < 0)
+    {
+        Console.WriteLine("Введено ненатуральное число");
+        return false;
+    }
+    else return true;
+}
+
+int Power(int arg1, int arg2)
+{
+    int result = 1;
+    for (int i = 0 ; i < arg2; i++)
+    {
+        result *= arg1;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите показатель степени");
+int step = Convert.ToInt32(Console.ReadLine());
+if (Check(num))
+{
+    Console.WriteLine($"{num}, {step} -> {Power(num, step)}");
+}
