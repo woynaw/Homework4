15e6ede2db76bd172da74483d632e05898f6e5d8
Войналович Владислав <woynaw@hotmail.com>
2023-05-25 12:33:07 +0300

    add Zadacha27

--- Zadacha27/Program.cs
+++ Zadacha27/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int SumDig(int arg)
+{
+    int sum = 0;
+    while (arg > 0)
+    {
+        sum += arg % 10;
+        arg /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num} -> {SumDig(num)}");
