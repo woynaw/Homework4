9950bed6c5b63c18e504827c6245b31af8e248b0
Войналович Владислав <woynaw@hotmail.com>
2023-05-25 12:51:30 +0300

    add Zadacha29

--- Zadacha29/Program.cs
+++ Zadacha29/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] RandomArray(int len, int min, int max)
+{
+    int[] array = new int[len];
+    Random random = new Random();
+    for (int i = 0; i < len - 1; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+
+    return array;
+}
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length-1]}");
+    System.Console.Write("]");
+}
+
+
+
+Console.WriteLine("Введите размер массива");
+int len = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите левую границу");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите правую границу");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = RandomArray(len, min, max);
+PrintArray(array);
